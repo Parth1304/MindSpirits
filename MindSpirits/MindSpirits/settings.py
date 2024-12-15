cc1dcfc397dc638abad03b5417439d51daac898e
Parth1304 <97118975+Parth1304@users.noreply.github.com>
2024-12-15 21:57:06 +0530

    testing

--- MindSpirits/MindSpirits/settings.py
+++ MindSpirits/MindSpirits/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-no&ne=dv(1e8t+o-fvk(9lg=9=ms7884+dd=jkm6&h($ok_)!e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
